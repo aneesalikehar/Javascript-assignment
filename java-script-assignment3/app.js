db66a06e1cbfc19a09a3ace51e55cb3fad56c7f3
aneesalikehar <134583553+aneesalikehar@users.noreply.github.com>
2023-08-20 23:12:25 -0700

    commitjavascriptassignment3

---
+++ java-script-assignment3/app.js
@@ -0,0 +1,140 @@
+// var a = ["pakistan", "india", "afghanistan",]
+
+// console.log(a);
+
+// var a = "anees"
+// var b = "ali"
+
+// var students =["anees", "ali", "maam"];
+
+// console.log(students[0])
+
+// var students = [];
+
+// .....................chapter 14 to 16 ............................
+// Q8
+// var students = [];
+// var students = ();
+// var students = ["anees", "ali","maam"];
+// var number = [1,2,3,4,5];
+// var boolean = [true,false,true];
+// var mixedArray = [1,"anees",true]
+
+// var a = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "MPhil", "PhD"];
+
+
+// a[a.length]
+// document.write("<h1>Qualifications</h1>");
+// document.write("<ul>");
+// document.write("<li>" + a + "</li>");
+// document.write("</ul>")
+
+// var studentsname = ["Anees","Ali","Ahmed"];
+
+// var score = [380,420,350];
+// var totalMarks = 500;
+
+// document.write("Score of " + studentsname[0] +" is" + score[0]+".percentage:" + (score[0]/500) * 100 + "%");
+// document.write("<br>"+" Score of " + studentsname[1] + " is" + score [1]+".percentage" + (score[1]/500) *100 + "%");
+// document.write("<br>" + "Score of " + studentsname[2]+"is "+"percentage" + (score[2]/500)*100 + "%"); 
+
+// Q9
+//  var color = ["Red", "Green","blue"];
+// document.write("<h2>Actual Array:" +color+ "</h2>");
+
+// var colorAddStart = prompt("Enter Color into the begning");
+// color.unshift(colorAddStart);
+// document.write(color);
+
+// var colorAddEnd = prompt("Enter Color into the begning");
+// color.push(colorAddEnd);
+// document.write("</br>")
+// document.write(color);
+
+// var colorAddStart = prompt("Enter Color into the begning");
+// var colorAddStart1 = prompt("Enter Color into the begning");
+// color.unshift(colorAddStart,colorAddStart1);
+// document.write("<br>")
+// document.write(color);
+
+// color.pop()
+// document.write("</br>")
+// document.write("Array after deleting the first color",color);
+
+// color.shift(color)
+// document.write("</br>")
+// document.write("Array after deleting the last color",color);
+
+// var colorIndex = prompt("which index he/she wants to add a color");
+// var newColorAtIndex = prompt("Enter the color to add at the specified index:")
+// color.splice(colorIndex,0,newColorAtIndex);
+// document.write("<br>")
+// document.write(color);
+
+// var deletindex = prompt("Enter the index from which you want to delete color")
+// var deletcount = prompt("Enter the color count to delete");
+// color.splice(deletindex,deletcount)
+// document.write("<br>")
+// document.write(color);
+
+
+//Q10
+// var a = [320,230,480,120]
+// document.write("Original Array" + a);
+// a.sort()
+// document.write("<br>")
+// document.write("Ordered Scores of Students" + a)
+
+//Q11
+// var cityName = ["Karachi","Lahore","Islamabad","Multan","Quetta","Peshawer"];
+// document.write("<h1>Cities List</h1>"+ cityName);
+// document.write("<h1>Selected Cities List</h1>")
+// var a = cityName.slice(2,5)
+// document.write(a)
+
+//12
+// var arr =["This","is","my","car"];
+
+// var result = arr.join(" ")
+// document.write(result)
+
+//Q13
+// var arry = [];
+// document.write("<h1>Devices</h1>");
+// var a = arry.push("Keyboard");
+// var b = arry.push("Mouse");
+// var c = arry.push("Printer");
+// var d = arry.push("Monitor");
+// document.write(arry);
+
+
+// var a = arry.shift();
+// document.write("<br><br>Out:<br>" + a);
+// var b = arry.shift();
+// document.write("<br><br>Out:<br>" + b)
+// var c = arry.shift();
+// document.write("<br><br>Out:<br>" + c)
+// var d = arry.shift();
+// document.write("<br><br>Out:<br>" + d)
+
+
+//Q15
+// var arry = []
+// var a = arry.unshift("Nokia");
+// var b = arry.unshift("Apple");
+// var c = arry.unshift("Samsung");
+// var d = arry.unshift("Motorola");
+// var e = arry.unshift("Sony & Haier");
+// document.write(arry);
+// document.write("</br>")
+
+// document.write("<select><option>Select Disable</option><option>" + arry[0] + "</option><option>" + arry[1] + "</option><option>" + arry[2] + "</option><option>" + arry[3] + "</option><option>" + arry[4] + "</option></select>")
+
+
+
+
+
+
+
+
+
